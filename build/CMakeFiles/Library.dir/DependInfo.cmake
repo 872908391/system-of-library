d6efdbe44f8214653e292f8965d49a61b902b227
DONG <872908391@qq.com>
2021-10-30 19:14:55 +0800

    环境配置

---
+++ build/CMakeFiles/Library.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "H:/CODE/Codefiled_c++/Library/main.cpp" "CMakeFiles/Library.dir/main.obj" "gcc" "CMakeFiles/Library.dir/main.obj.d"
+  "H:/CODE/Codefiled_c++/Library/src/boo.cpp" "CMakeFiles/Library.dir/src/boo.obj" "gcc" "CMakeFiles/Library.dir/src/boo.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Library.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Library.dir/main.obj"
+  "CMakeFiles/Library.dir/main.obj.d"
+  "CMakeFiles/Library.dir/src/boo.obj"
+  "CMakeFiles/Library.dir/src/boo.obj.d"
+  "Library.exe"
+  "Library.exe.manifest"
+  "Library.pdb"
+  "libLibrary.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
